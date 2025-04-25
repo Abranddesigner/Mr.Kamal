f211d0be90026ae8b4f8b950689be496bbc54717
Mr. Kamal Meena <ideogram682@gmail.com>
2025-04-25 17:16:12 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,14 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
     observer.observe(section);
   });
 
-  // Back to Top
-  const backToTop = document.querySelector('.back-to-top');
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) backToTop.classList.add('show');
-    else backToTop.classList.remove('show');
-  });
-  backToTop.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
-
   // Menu Toggle
   const menuToggle = document.querySelector('.menu-toggle');
   const navbar = document.querySelector('#navbar');
